8a003e9f84aaa15e0dabff517f498b79de502542
codeace110 <neri11032321@gmail.com>
2023-06-13 02:33:06 +0800

    i added or install sqlite for storing stars

--- js/main.js
+++ js/main.js
@@ -119,13 +119,13 @@ fetch(`https://api.github.com/users/${username}/repos?sort=updated&direction=des
       card.appendChild(language);
     
       const lastUpdate = document.createElement('span');
-      lastUpdate.textContent = `Updated ${getTimeAgo(repository.updated_at)}`;
+      lastUpdate.textContent = `  Updated ${getTimeAgo(repository.updated_at)}..   `;
       lastUpdate.classList.add('last-update');
       card.appendChild(lastUpdate);
     
       const demoLink = document.createElement('a');
       demoLink.href = repository.homepage;
-      demoLink.textContent = 'View Demo';
+      demoLink.textContent = '  View Demo';
       demoLink.classList.add('demo-link');
       card.appendChild(demoLink);
     
@@ -163,6 +163,168 @@ fetch(`https://api.github.com/users/${username}/repos?sort=updated&direction=des
     } else {
       return `${hoursDiff} hours ago`;
     }
-  };
-  
-  
+  };;
+
+
+  /**Repository Search Script */
+
+const searchInput = document.getElementById('search-input');
+const searchButton = document.getElementById('search-button');
+
+fetch(`https://api.github.com/users/${username}/repos?sort=updated&direction=desc`)
+  .then(response => response.json())
+  .then(data => {
+    const repositories = data;
+    displayRepositories(repositories);
+
+    searchButton.addEventListener('click', handleSearch);
+    searchInput.addEventListener('input', handleSearch);
+
+    function handleSearch(event) {
+      event.preventDefault();
+      const keyword = searchInput.value.toLowerCase();
+      const filteredRepositories = repositories.filter(repository =>
+        repository.name.toLowerCase().includes(keyword)
+      );
+      displayRepositories(filteredRepositories);
+    }
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
+function displayRepositories(repositories) {
+  repositoriesContainer.innerHTML = '';
+
+  repositories.forEach(repository => {
+    const card = createRepositoryCard(repository);
+    repositoriesContainer.appendChild(card);
+  });
+};;
+
+/***github commits and update*/
+
+const commitsContainer = document.getElementById('commits');
+const paginationContainer = document.getElementById('pagination');
+const commitsPerPage = 5;
+let currentPage = 1;
+let commitsData = [];
+
+fetch(`https://api.github.com/users/${username}/events`)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Failed to fetch commits');
+    }
+    return response.json();
+  })
+  .then(data => {
+    commitsData = data.filter(event => event.type === 'PushEvent');
+    displayCommits(commitsData);
+    displayPagination(commitsData.length);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+    commitsContainer.textContent = 'Failed to fetch commits';
+  });
+
+function displayCommits(commits) {
+  commitsContainer.innerHTML = '';
+
+  const startIndex = (currentPage - 1) * commitsPerPage;
+  const endIndex = startIndex + commitsPerPage;
+  const commitsToDisplay = commits.slice(startIndex, endIndex);
+
+  commitsToDisplay.forEach(commit => {
+    const commitCard = createCommitCard(commit);
+    commitsContainer.appendChild(commitCard);
+  });
+}
+
+function createCommitCard(commit) {
+  const commitCard = document.createElement('div');
+  commitCard.classList.add('commit-card');
+
+  const repoName = document.createElement('h4');
+  const repoLink = document.createElement('a');
+  repoLink.href = `https://github.com/${commit.repo.name}`;
+  repoLink.textContent = commit.repo.name;
+  repoName.appendChild(repoLink);
+  commitCard.appendChild(repoName);
+
+  const commitMessage = document.createElement('p');
+  commitMessage.textContent = commit.payload.commits[0].message;
+  commitCard.appendChild(commitMessage);
+
+  const commitDate = document.createElement('span');
+  const commitTime = new Date(commit.created_at);
+  commitDate.textContent = commitTime.toDateString();
+  commitCard.appendChild(commitDate);
+
+  return commitCard;
+}
+
+function displayPagination(totalCommits) {
+  paginationContainer.innerHTML = '';
+
+  const totalPages = Math.ceil(totalCommits / commitsPerPage);
+
+  for (let i = 1; i <= totalPages; i++) {
+    const button = document.createElement('button');
+    button.classList.add('pagination-button');
+    button.textContent = i;
+    button.addEventListener('click', () => {
+      currentPage = i;
+      displayCommits(commitsData);
+      updatePaginationButtons();
+    });
+    paginationContainer.appendChild(button);
+  }
+
+  updatePaginationButtons();
+}
+
+function updatePaginationButtons() {
+  const buttons = paginationContainer.getElementsByTagName('button');
+
+  for (let i = 0; i < buttons.length; i++) {
+    if (i + 1 === currentPage) {
+      buttons[i].classList.add('active');
+    } else {
+      buttons[i].classList.remove('active');
+    }
+  }
+
+  buttons[0].disabled = currentPage === 1;
+  buttons[buttons.length - 1].disabled = currentPage === buttons.length;
+}
+
+/***scrip for stars and badge */
+
+const starLink = document.getElementById('starLink');
+const starBadge = document.getElementById('starBadge');
+let isStarred = false;
+
+starLink.addEventListener('click', () => {
+  if (!isStarred) {
+    incrementStarCount();
+    isStarred = true;
+  }
+});
+
+function incrementStarCount() {
+  const currentCount = parseInt(starBadge.textContent);
+  const newCount = currentCount + 1;
+  starBadge.textContent = newCount;
+
+  // Store the updated star count in localStorage
+  localStorage.setItem('starCount', newCount);
+}
+
+// Retrieve the star count from localStorage on page load
+document.addEventListener('DOMContentLoaded', () => {
+  const storedCount = localStorage.getItem('starCount');
+  if (storedCount) {
+    starBadge.textContent = storedCount;
+    isStarred = true;
+  }
+});
